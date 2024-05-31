ed84cddc7a20a8d3129443bf5202d5a18ba1f856
Miŕko con Pritty <d48669816@alumnos.itr.edu.ar>
2024-05-31 09:24:53 -0300

    Ahi Hay Algo A.H.A

--- Lajoya.js
+++ Lajoya.js
@@ -1,5 +1,41 @@
-let Bichin;
+let Cubin;
+let CuboSi;
+let CubiNo;
+let Tocado;
 function setup() {
-createCanvas(windowWidth, windowHeight);
-background(255)
+  createCanvas(windowWidth, windowHeight);
+  Tocado = createSprite(
+    width-300, height-600, 120, 120);
+   Tocado.shapeColor = color(0);
+  CuboSi = createSprite(
+    width/2, height/2, 100, 100);
+  CuboSi.shapeColor = color(250,219,216);
+  CuboNo = createSprite(
+    width/4, height/3, 100, 100);
+  CuboNo.shapeColor = color(200,100,216);
+  Cubin = createSprite(0, 0, 50, 50);
+  Cubin.shapeColor = color(128);
+}
+function draw() {
+  background(50);
+  Cubin.velocity.x = (mouseX-Cubin.position.x)*0.2;
+  Cubin.velocity.y = (mouseY-Cubin.position.y)*0.2;
+  Cubin.displace(CuboSi);
+  Cubin.displace(CuboNo);
+  CuboSi.displace(CuboNo);
+  drawSprites();
+
+  if (CuboSi.overlap(Tocado)) {
+    Tocado.shapeColor = color(255);
+  }
+  else {
+    Tocado.shapeColor = color(0);
+  }
+
+  if (CuboNo.overlap(Tocado)) {
+    Tocado.shapeColor = color(255, 0, 0);
+  }
+  else {
+    Tocado.shapeColor = color(0);
+  }
 }
